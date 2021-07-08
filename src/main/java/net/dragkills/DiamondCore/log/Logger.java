269d69f7e37999da9d26c565f35b27dd61f1b60a
DRAGKILLS <70339357+DRAGKILLS@users.noreply.github.com>
2021-07-08 17:15:14 +0300

    Update Logger.java

--- src/main/java/net/dragkills/DiamondCore/log/Logger.java
+++ src/main/java/net/dragkills/DiamondCore/log/Logger.java
@@ -5,4 +5,8 @@ public class Logger {
     public static void info(String message) {
         System.out.println("-> " + message);
     }
+
+    public static void info_normal(String message) {
+        System.out.println(message);
+    }
 }
